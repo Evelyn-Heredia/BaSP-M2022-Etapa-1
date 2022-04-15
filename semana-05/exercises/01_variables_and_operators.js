158a435e83ef57cec96324680ffa63991b813d0a
Evelyn Heredia <evefhrd@gmail.com>
2022-04-15 08:57:22 -0300

    BaSP-W05-2022: Ex01-variables and operators

--- semana-05/exercises/01_variables_and_operators.js
+++ semana-05/exercises/01_variables_and_operators.js
@@ -1,1 +1,17 @@
-console.log ('Testing JS script');
+// Test
+console.log('Testing JS script');
+//Exercise 1a.
+var firstValue = 100;
+var secondValue = 80;
+var result = firstValue + secondValue;
+console.log('The sum of 100 and 80 is equal to ', result);
+//Exercise 1b.
+var firstName = 'Tom';
+var surname = 'Rider';
+var fullName = firstName + ' ' + surname;
+console.log('His full name is', fullName);
+//Exercise 1c.
+var firstItem = 'This is a';
+var secondItem = 'string of';
+var allItems = firstItem.length + secondItem.length;
+console.log('This is a string of', allItems, 'code units long.');
