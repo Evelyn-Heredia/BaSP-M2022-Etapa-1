9f757fb16f21d2e2bfbd4d8a75fbb0ab381e3bb4
Evelyn Heredia <evefhrd@gmail.com>
2022-04-24 12:42:22 -0300

    BaSP-W06-2022: Attempt for email validation on log in form

--- semana-06/javascript/login.js
+++ semana-06/javascript/login.js
@@ -1,3 +1,14 @@
 window.onload = function(){
-		
+	function validation(e){
+        var regEx = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+        var validEmail = regEx.test(e);
+        if (validEmail=true){
+            alert('right email, poner cartel verde');
+        } else{
+            alert('Error: invalid Email.')
+        }
+    }
+    var emailBtn = document.querySelector('form #eMail');
+    emailBtn.addEventListener(blur, validation(emailBtn.value.target));
+    
 }
