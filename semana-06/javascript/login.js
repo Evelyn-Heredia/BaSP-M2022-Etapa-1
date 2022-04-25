00b2b734ef8c76d473c0006807f73b08cfbf1f0a
Evelyn Heredia <evefhrd@gmail.com>
2022-04-25 16:56:06 -0300

    BaSP-W06-2022: Log in input field validated

--- semana-06/javascript/login.js
+++ semana-06/javascript/login.js
@@ -1,14 +1,33 @@
 window.onload = function(){
-	function validation(e){
-        var regEx = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
-        var validEmail = regEx.test(e);
-        if (validEmail=true){
-            alert('right email, poner cartel verde');
-        } else{
-            alert('Error: invalid Email.')
-        }
+//Form field container
+var form = document.getElementById('form');
+//Input field
+var inputs = document.querySelectorAll('#form input');
+//Email field regex
+var regExMail = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
+//Validation function
+var formValidation = function(e){
+    if(regExMail.test(inputs[0].value)){
+        document.getElementById('mail-div').classList.remove('form-group-wrong');
+        document.getElementById('mail-div').classList.add('form-group-ok');
+        document.querySelector('#mail-div .form-input-error').classList.remove('form-input-error-active');
+    } else{
+        document.getElementById('mail-div').classList.add('form-group-wrong');
+        document.getElementById('mail-div').classList.remove('form-group-ok');
+        document.querySelector('#mail-div .form-input-error').classList.add('form-input-error-active');
     }
-    var emailBtn = document.querySelector('form #eMail');
-    emailBtn.addEventListener(blur, validation(emailBtn.value.target));
-    
+};
+// Reset input field
+function resetForm(e){
+    document.getElementById('mail-div').classList.remove('form-group-wrong');
+    document.querySelector('#mail-div .form-input-error').classList.remove('form-input-error-active');
+}
+//Email field validation
+inputs[0].addEventListener('blur', formValidation);
+inputs[0].addEventListener('focus', resetForm);
+// Submit button
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+});
+//Password field validation
 }
