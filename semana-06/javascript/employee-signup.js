0f46f658ced66e77a96b274cc61e96d55b51eba7
Evelyn Heredia <evefhrd@gmail.com>
2022-04-26 19:45:03 -0300

    BaSP-W06-2022: Employee HTML file structured

--- semana-06/javascript/employee-signup.js
+++ semana-06/javascript/employee-signup.js
@@ -1,3 +1,61 @@
 window.onload = function(){
-    console.log('Testing');
+////////// Variables //////////
+//Form label
+var form = document.getElementById('em-form');
+//Input field
+var inputs = document.querySelectorAll('#em-form input');
+//////////////////////////////////////////////////////////////////////
+//Validation function for Name
+var nameValidation = function(e){
+    if ( inputs[0] == null || inputs[0].length == 0){
+        document.getElementById('em-name-div').classList.add('em-form-group-wrong');
+        document.getElementById('em-name-div').classList.remove('em-form-group-ok');
+        document.querySelector('#em-name-div .em-form-input-error').classList.add('em-form-input-error-active');
+        //para usarlo para el boton submit
+        return true
+    } else{
+        document.getElementById('em-name-div').classList.add('em-form-group-wrong');
+        document.getElementById('em-name-div').classList.remove('em-form-group-ok');
+        document.querySelector('#em-name-div .em-form-input-error').classList.add('em-form-input-error-active');
+        //para usarlo para el boton submit
+        return false
+    }
+}
+// Reset input field function for Name
+function resetForm(e){
+    document.getElementById('mail-div').classList.remove('em-form-group-wrong');
+    document.getElementById('mail-div').classList.remove('em-form-group-ok');
+    document.querySelector('#mail-div .em-form-input-error').classList.remove('em-form-input-error-active');
+}
+//Name field validation
+inputs[0].addEventListener('blur', nameValidation);
+inputs[0].addEventListener('focus', resetForm);
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+//Validation function for Surame
+var surnameValidation = function(e){
+    if ( inputs[1] == null || inputs[1].length == 0){
+        document.getElementById('em-surname-div').classList.remove('em-form-group-wrong');
+        document.getElementById('em-surname-div').classList.add('em-form-group-ok');
+        document.querySelector('#em-surname-div .em-form-input-error').classList.remove('em-form-input-error-active');
+        //para usarlo para el boton submit
+        return false
+    } else{
+        document.getElementById('em-surname-div').classList.add('em-form-group-wrong');
+        document.getElementById('em-surname-div').classList.remove('em-form-group-ok');
+        document.querySelector('#em-surname-div .em-form-input-error').classList.add('em-form-input-error-active');
+        //para usarlo para el boton submit
+        return true
+    }
+}
+// Reset input field function for surname
+function resetForm(e){
+    document.getElementById('surname-div').classList.remove('em-form-group-wrong');
+    document.getElementById('surname-div').classList.remove('em-form-group-ok');
+    document.querySelector('#surname-div .em-form-input-error').classList.remove('em-form-input-error-active');
+}
+//Surname field validation
+inputs[1].addEventListener('blur', surnameValidation);
+inputs[1].addEventListener('focus', resetForm);
+//////////////////////////////////////////////////////////////////////
 }
--- semana-06/javascript/login.js
+++ semana-06/javascript/login.js
@@ -1,12 +1,12 @@
 window.onload = function(){
-//Form field container
+//Form label
 var form = document.getElementById('form');
 //Input field
 var inputs = document.querySelectorAll('#form input');
 //Email field regex
 var regExMail = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
-//Validation function
-var formValidation = function(e){
+//Validation function for Email
+var mailValidation = function(e){
     if(regExMail.test(inputs[0].value)){
         document.getElementById('mail-div').classList.remove('form-group-wrong');
         document.getElementById('mail-div').classList.add('form-group-ok');
@@ -16,18 +16,54 @@ var formValidation = function(e){
         document.getElementById('mail-div').classList.remove('form-group-ok');
         document.querySelector('#mail-div .form-input-error').classList.add('form-input-error-active');
     }
-};
-// Reset input field
+}
+// Reset input field function for Email
 function resetForm(e){
     document.getElementById('mail-div').classList.remove('form-group-wrong');
+    document.getElementById('mail-div').classList.remove('form-group-ok');
     document.querySelector('#mail-div .form-input-error').classList.remove('form-input-error-active');
 }
 //Email field validation
-inputs[0].addEventListener('blur', formValidation);
+inputs[0].addEventListener('blur', mailValidation);
 inputs[0].addEventListener('focus', resetForm);
-// Submit button
-form.addEventListener('submit', function(e){
-    e.preventDefault();
-});
-//Password field validation
+//////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+// Letters
+var letters = ["a","b","c","d","e","f","g","h","i","j","k",
+    "l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", 
+    "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P",
+    "Q","R","S","T","U","V","W","X","Y","Z"];
+// Numbers
+var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+// Symbols
+var symbols = ["!","#","$","%","&","/","(",")","=","?","¡","¿","+","*","[","]","{","}","-",".", "@"];
+//Password content test
+function passwordValidation(e){
+    var lettersLength = 0;
+    var digitsLength = 0;
+    var symbolsLength = 0;
+    for(var i= 0; i < inputValue.length; i++){
+        var inputName = inputValue[i]
+        if (letters.indexOf(inputName) != -1){
+            lettersLength++;
+        } else if (numbers.indexOf(inputName) != -1){
+            digitsLength++;
+        } else if (symbols.indexOf(inputName) != -1){
+            symbolsLength++;
+        }
+    }
+}
+if(numberOfLetters > 4 && numericChar > 2 && numberOfSymbols === 0) {
+    document.getElementById('password-div').classList.remove('form-group-wrong');
+    document.getElementById('password-div').classList.add('form-group-ok');
+    document.querySelector('#password-div .form-input-error').classList.remove('form-input-error-active');
+} else {
+    document.getElementById('password-div').classList.add('form-group-wrong');
+    document.getElementById('password-div').classList.remove('form-group-ok');
+    document.querySelector('#password-div .form-input-error').classList.add('form-input-error-active');
+}
+//Password validation
+inputs[1].addEventListener('blur', passwordValidation);
+inputs[1].addEventListener('focus', resetForm);
+
 }
